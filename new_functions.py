a1c55a36d7068c2b002fe08a1a46f25a9d1115de
aswanipranjal <aswani.pranjal@gmail.com>
2018-06-06 20:05:52 +0530

    Created Calculating CHAOSS metrics notebook and updated new_functions.py file to calculate the current(CHAOSS) metrics that manuscripts calculates by default.

--- new_functions.py
+++ new_functions.py
@@ -32,18 +32,42 @@
 # are in all the classes because we do not calculate all the aggregations for every 
 # Item(Issue, Commit, PR).
 
-import pandas as pd
-from datetime import datetime
+import sys
+import logging
 
+from collections import OrderedDict
+from datetime import datetime, timezone
+
+import pandas as pd
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import A, Q, Search
 
-import sys
-import logging
+
 
 logger = logging.getLogger(__name__)
 
 # ------ New Classes and functions ------ #
+
+# here is how the class can be used:
+# from new_functions import Metric
+# >> github = Metric(url, index, start, end)
+# 
+# >> github.cardinaliry("users") 
+# now this will add a user aggregation to the github object
+# >> github.by_authors()
+# this will pop the last added aggregation from the aggregation list 
+# seggregate the aggreagtion by user buckets.
+# 
+# >> github.get_cardinality("commits").by_authors() # OR
+# gives us the number of commits by authors
+# >> github.min("author_date").by_authors()
+# this should give us the min date i.e the first dates when the authors made their 
+# first commits and from that we can calculate the new users per month
+
+# DEVELOPER's NOTE:
+# Some of the parameters for function definitions have been referenced from the 
+# old esquery.py file, because they are the correct implementation for that function.
+
 
 class Metric():
     """
@@ -57,7 +81,8 @@ class Metric():
     allow us to neatly add filters, create nested aggregations and such. 
     """
 
-    def __init__(self, url=None, index=None):
+    def __init__(self, url=None, index=None, start=None, end=None, esfilters={},
+                 interval=None, offset=None):
         """Initialization of necessary parameters
     
         :client: the address/url of the elasticsearch instance to be used?
@@ -71,7 +96,7 @@ class Metric():
             self.es = Elasticsearch(url)
         else:
             logger.debug("No custom url provided, using default: http://localhost:9200/")
-            self.es = Elasticsearch()
+            self.es = Elasticsearch("http://localhost:9200/")
     
         if not index:
             logger.error("Please provide an index to query!")
@@ -81,11 +106,15 @@ class Metric():
         self.s = Search(using=self.es, index=self.index)
     
         self.queries = {"must":[], "must_not":[]}
-        self.filters = []
-        self.aggregations = []
+        self.filters = {}
+        self.filters.update(esfilters)
+        self.aggregations = OrderedDict()
         self.size = 10000 # temporary hack to get all the data
-        self.period = "month" # default period to create buckets from in date histogram
         self.precision_threshold = 3000 # accuracy that we want when counting the number of items
+        self.start = start
+        self.end = end
+        self.interval = interval if interval else "month"
+        self.offset = offset
 
     def add_query(self, key_val={}):
         q = Q("match", **key_val)
@@ -98,63 +127,13 @@ class Metric():
         self.queries['must_not'].append(q)
 
     def show_queries(self):
-        return self.s.to_dict()['query']
+        return self.queries
 
     def increment_parent(self):
         self.parent_id += 1
 
-    def get_results(self):
-        response = self.s.execute()
-        aggregations = response.aggregations.to_dict()
-        agg_list = []
-        for i in range(self.parent_id):
-            try:
-                agg_list.append(buckets_to_df(aggregations[str(i)]['buckets']))
-            except:
-                agg_list.append(aggregations[str(i)])
-        return agg_list
-
-    def by_authors(self):
-        # here, for commits, should we bucket by committer or by author?
-        agg = A("terms", field="author_name", missing="others", size=self.size)
-        try:
-            # for issues and PRs
-            agg.metric(self.child_id, "cardinality", field="id_in_repo")
-        except:
-            # for commits
-            agg.metric(self.child_id, "cardinality", field="hash")
-        #else:
-        #    logger.error("Invalid es index to apply 'by_authors' filter!")
-        self.s.aggs.bucket(self.parent_id, agg)
-        self.s = self.s.extra(size=0)
-        self.increment_parent()
-        self.aggregations.append(agg)
-        return self
-
-    def by_organizations(self):
-        agg = A("terms", field="user_org", missing="others", size=self.size)
-        # the git enrich index does not mention the user org anywhere.
-        # TODO: open an issue after this?
-        # this functions is currently only for issues and PRs
-        agg.metric(self.child_id, "cardinality", field="id_in_repo")
-        self.s.aggs.bucket(self.parent_id, agg)
-        self.s = self.s.extra(size=0)
-        self.increment_parent()
-        self.aggregations.append(agg)
-        return self
-
-    def by_period(self):
-        try:
-            agg = A("date_histogram", field="created_at", interval=self.period)
-            agg.metric(self.child_id, "cardinality", field="id_in_repo")
-        except:
-            agg = A("date_histogram", field="commit_date", interval=self.period)
-            agg.metric(self.child_id, "cardinality", field="hash")
-        self.s.aggs.bucket(self.parent_id, agg)
-        self.s = self.s.extra(size=0)
-        self.increment_parent()
-        self.aggregations.append(agg)
-        return self
+    def increment_child(self):
+        self.child_id += 0.1
 
     def is_open(self):
         query = {"state":"open"}
@@ -164,12 +143,222 @@ class Metric():
         query = {"state":"closed"}
         self.add_query(query)
 
+    def get_sum(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("sum", field=field)
+        self.aggregations['sum_' + field] = agg
+        return self
 
-class Git(Metric):
-    pass
+    def get_average(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("avg", field=field)
+        self.aggregations['avg_' + field] = agg
+        return self
 
+    def get_percentile(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("percentiles", field=field)
+        self.aggregations['percentile_' + field] = agg
+        return self
+
+    def get_terms(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("terms", field=field, size=self.size, order={"_count":"desc"})
+        self.aggregations['terms_' + field] = agg
+        return self
+
+    def get_min(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("min", field=field)
+        self.aggregations['min_' + field] = agg
+        return self
+
+    def get_max(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("max", field=field)
+        self.aggregations['max_' + field] = agg
+        return self
+
+    def get_cardinality(self, field=None):
+        if not field:
+            raise AttributeError("Please provide field to apply aggregation to!")
+        agg = A("cardinality", field=field, precision_threshold=self.precision_threshold)
+        self.aggregations['cardinality_' + field] = agg
+        return self
+
+    def set_range(self, date_field=None, start=None, end=None):
+        start = start if start else self.start
+        end = end if end else self.end
+
+        if not date_field:
+            date_field = "grimoire_creation_date"
+        start_end = {}
+        start_end["gte"] = "%s" % start.isoformat()
+        start_end["lte"] = "%s" % end.isoformat()
+        query_range = {date_field: start_end}
+        self.s = self.s.filter("range", **query_range)
+        return self
+
+    def by_authors(self, field=None):
+        # here, for commits, should we bucket by committer or by author?
+        # Parent aggregation
+        agg_field = field if field else "author_name"
+        agg = A("terms", field=agg_field, missing="others", size=self.size)
+        # child aggregation is the last added aggregation
+        child_agg = self.aggregations.popitem()
+        # add child agg to parent agg
+        agg.metric(self.child_id, child_agg[1])
+        # insert this agg to the agg dict. This agg essentially replaces
+        # the last agg that was in the agg dict
+        self.aggregations["terms_" + agg_field] = agg
+        return self
+
+    def by_organizations(self, field=None):
+        # the git enrich index does not mention the user org anywhere.
+        # TODO: open an issue after this?
+        # this functions is currently only for issues and PRs
+        agg_field = field if field else "user_org"
+        agg = A("terms", field=agg_field, missing="others", size=self.size)
+        child_agg = self.aggregations.popitem()
+        agg.metric(self.child_id, child_agg[1])
+        self.aggregations["terms_" + agg_field] = agg
+        return self
+
+    def by_period(self, period=None, timezone=None, field=None, start=None, end=None):
+        
+        hist_period = period if period else self.interval
+        time_zone = timezone if timezone else "UTC"
+        start = start if start else self.start
+        end = end if end else self.end
+        bounds = self.get_bounds(start, end)
+        date_field = field if field else "grimoire_creation_date"
+        agg = A("date_histogram", field=date_field, interval=hist_period,
+                time_zone=time_zone, min_doc_count=0, **bounds)
+
+        child_agg = self.aggregations.popitem()
+        agg.metric(self.child_id, child_agg[1])
+        self.aggregations["date_histogram_" + child_agg[0]] = agg
+        return self
+
+    def get_bounds(self, start=None, end=None):
+
+        # Extend bounds so we have data until start and end
+        start = start.replace(microsecond=0)
+        start_ts = start.replace(tzinfo=timezone.utc).timestamp()
+        start_ts_ms = start_ts * 1000  # ES uses ms
+        end = end.replace(microsecond=0)
+        end_ts = end.replace(tzinfo=timezone.utc).timestamp()
+        end_ts_ms = end_ts * 1000  # ES uses ms
+        bounds_data = {"min": start_ts_ms, "max": end_ts_ms}
+        bounds = {"extended_bounds": bounds_data}
+        return bounds
+
+    def get_results(self, dataframe=False):
+        """
+        dataframe: if true, return values as a pandas dataframe
+        """
+        for key, val in self.aggregations.items():
+            self.s.aggs.bucket(self.parent_id, val)
+            self.increment_parent()
+        
+        self.s = self.s.extra(size=0)
+        response = self.s.execute()
+        return response.to_dict()
+
+    def get_ts(self):
+        res = self.get_results()
+
+        ts = {"date": [], "value": [], "unixtime": []}
+
+        if 'buckets' not in res['aggregations'][str(self.parent_id-1)]:
+            raise RuntimeError("Aggregation results have no buckets in time series results.")
+
+        for bucket in res['aggregations'][str(self.parent_id-1)]['buckets']:
+            ts['date'].append(bucket['key_as_string'])
+            if str(self.child_id) in bucket:
+                # We have a subaggregation with the value
+                # If it is percentiles we get the median
+                if 'values' in bucket[str(self.child_id)]:
+                    val = bucket[str(self.child_id)]['values']['50.0']
+                    if val == 'NaN':
+                        # ES returns NaN. Convert to None for matplotlib graph
+                        val = None
+                    ts['value'].append(val)
+                else:
+                    ts['value'].append(bucket[str(self.child_id)]['value'])
+            else:
+                ts['value'].append(bucket['doc_count'])
+            # unixtime comes in ms from ElasticSearch
+            ts['unixtime'].append(bucket['key'] / 1000)
+
+        return ts
+
+    def get_aggs(self):
+        res = self.get_results()
+        if 'aggregations' in res and 'values' in res['aggregations'][str(self.parent_id-1)]:
+            try:
+                agg = res['aggregations'][str(self.parent_id-1)]['values']["50.0"]
+                if agg == 'NaN':
+                    # ES returns NaN. Convert to None for matplotlib graph
+                    agg = None
+            except:
+                raise RuntimeError("Multivalue aggregation result not supported")
+        elif 'aggregations' in res and 'value' in res['aggregations'][str(self.parent_id-1)]:
+            agg = res['aggregations'][str(self.parent_id-1)]['value']
+        else:
+            agg = res['hits']['total']
+
+        return agg
+
+
+    def get_trend(self):
+        ts = self.get_ts()
+        last = ts['value'][len(ts['value']) - 1]
+        prev = ts['value'][len(ts['value']) - 2]
+
+        trend = last - prev
+        trend_percentage = None
+        if last == 0:
+            if prev > 0:
+                trend_percentage = -100
+            else:
+                trend_percentage = 0
+        else:
+            trend_percentage = int((trend / last) * 100)
+
+        return (last, trend_percentage)
+
+    def show_search_object(self):
+        # This function is purely for debugging purposes
+        return self.s.to_dict()
 
 # ------- Helper functions ------ #
+
+def calculate_bmi(closed, submitted):
+    # takes as input 2 dictionaries, usually time series buckets generated by
+    # get_ts() function in the Metric class
+    if sorted(closed.keys()) != sorted(submitted.keys()):
+        raise AttributeError("The buckets supplied are not congruent!")
+
+    dates = closed['date']
+    closed_values = closed['value']
+    submitted_values = submitted['value']
+    ratios = []
+    for x, y in zip(closed_values, submitted_values):
+        if y==0:
+            ratios.append(0)
+        else:
+            ratios.append(x/y)
+    dates = ["{}-{}".format(date[:4], date[5:7]) for date in dates]
+    return {"Period":dates, "Closed/Submitted":ratios}
+
+
 def buckets_to_df(buckets):
     """ Takes in aggregation bucket objects and converts them into a pandas dataframe
     after cleaning the buckets. If a DateTime field is present(usually having the name:
@@ -199,6 +388,8 @@ def buckets_to_df(buckets):
         ret_df = pd.DataFrame.from_records(cleaned_buckets, index="key")
 
     return ret_df
+
+
 
 def term_buckets_to_df(buckets):
     pass
